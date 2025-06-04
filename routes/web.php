05a2c94444685a3fdc5820a286b275ac9d563683
= azariacindy <= cindysss099@gmail.com>
2025-06-04 19:09:24 +0700

    .feat iyeah

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\KriteriaController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\KriteriaController;
 /*use App
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,17 +16,6 @@ use App\Http\Controllers\KriteriaController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/', function () {
-    return view('landing');
-});
-Route::get('/login', function () {
-    return view('login');
-});
-Route::get('/register', function () {
-    return view('register');
-});
-
-Route::get('/kriteria', [KriteriaController::class, 'index'])->name('kriteria.index');
 
 // Route::pattern('id', '[0-9]+');
 // Route::get('/dasboard', [DashboardController::class, 'index']);
